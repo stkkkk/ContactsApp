9a3894af6eea1bc88951bbd8b21884e18f35a9fa
Yurii <yurii.stetsyk97@gmail.com>
2020-12-15 16:55:34 +0200

    initial commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,83 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  # GET /contacts
+  # GET /contacts.json
+  def index
+    @contacts = Contact.all
+  end
+
+  # GET /contacts/1
+  # GET /contacts/1.json
+  def show
+  end
+
+  # GET /contacts/new
+  def new
+    # @contact = Contact.new
+    @contact = current_user.contacts.build
+  end
+
+  # GET /contacts/1/edit
+  def edit
+  end
+
+  # POST /contacts
+  # POST /contacts.json
+  def create
+    # @contact = Contact.new(contact_params)
+    @contact = current_user.contacts.build(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.json { render :show, status: :created, location: @contact }
+      else
+        format.html { render :new }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /contacts/1
+  # PATCH/PUT /contacts/1.json
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
+        format.json { render :show, status: :ok, location: @contact }
+      else
+        format.html { render :edit }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /contacts/1
+  # DELETE /contacts/1.json
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.html { redirect_to contacts_url, notice: 'Contact was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def correct_user
+    @contact = current_user.contacts.find_by(id: params[:id])
+    redirect_to contacts_path, notice: "Not authorized to edit this contact!" if @contact.nil?
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email, :phone, :telegram, :user_id)
+    end
+end
